e43e8121340e91a8f8836ce2ccb984707b8c4983
panamini <panamini@tutamail.com>
2026-06-09 01:46:55 +0200

    fix(pr4): remove unsafe frame bridge export

--- modules/frame-analysis/android/src/main/java/com/normacamera/frameanalysis/NormaFrameAnalysisModule.kt
+++ modules/frame-analysis/android/src/main/java/com/normacamera/frameanalysis/NormaFrameAnalysisModule.kt
@@ -12,16 +12,11 @@ class NormaFrameAnalysisModule : Module() {
       NormaFrameAnalysisStore.analyzeDownsampledLumaGrid(values.toDoubleArray(), width, height, createdAt, parseValueRange(valueRange))
     }
 
-    Function("analyzeVisionCameraFrame") { frame: Any? ->
-      NormaVisionCameraFrameAnalyzer.analyzeFrame(frame)
-    }
-
     AsyncFunction("getLatestAnalysis") {
       NormaFrameAnalysisStore.getLatestAnalysis()
     }
 
     Function("reset") {
-      NormaVisionCameraFrameAnalyzer.reset()
       NormaFrameAnalysisStore.reset()
     }
   }
